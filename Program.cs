2dbd2d60c0120800bce18b9ab06d91b4afaa763c
Ishan-Chanuka <ickodippiliarachchi@students.nsbm.lk>
2021-07-02 23:28:15 +0530

    not complete

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace jump_search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int key = 7;
+
+            int result = Convert.ToInt32(jumpSearch(array, key));
+
+            Console.WriteLine(result);
+
+        }
+
+        static int jumpSearch(int[] arr, int key)
+        {
+            int len = arr.Length;
+
+            int jump = (int)Math.Floor(Math.Sqrt(len));
+
+
+            for (int i=0; i<len; i += jump)
+            {
+               
+            }
+            return -1;
+        }
+    }
+}
